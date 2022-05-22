097e18751271f41ac32c743f1a509bb45eda6e6c
Brian Chavarria <bayyand7@gmail.com>
2022-05-21 20:45:34 -0500

    Iphone icon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,8 +5,20 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <meta name="application-name" content="BeeReign" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="BeeReign" />
+        <meta
+          name="description"
+          content="Open source beekeeping traceability and inventory management software"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
       </Head>
       <body>
         <Main />
